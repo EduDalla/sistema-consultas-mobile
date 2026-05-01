46436a3490d498bda3ebaae1db0bb4954ae2b98a
Eduardo Dallabella <eduardodallabella@gmail.com>
2026-04-30 22:10:38 -0300

    chore: centraliza exports de componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 /**
  * Exportacao centralizada dos componentes da aplicacao.
  */
+
 export { default as ConsultaCard } from "./ConsultaCard";
+export { default as Loading } from "./Loading";
+export { default as EmptyState } from "./EmptyState";
