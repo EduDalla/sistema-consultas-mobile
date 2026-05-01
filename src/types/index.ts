e1f194321d7011d8e200d6a8fd5b6f6c9f8f0359
Eduardo Dallabella <eduardodallabella@gmail.com>
2026-04-30 22:10:38 -0300

    feat: centraliza exports de tipos

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export type { Consulta } from "../interfaces/consulta";
+export type { Especialidade } from "./especialidade";
+export type { Paciente } from "./paciente";
+export type { StatusConsulta } from "./statusConsulta";
+export type { TipoUsuario, Usuario } from "./usuario";
+
